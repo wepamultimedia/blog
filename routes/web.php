c7f928c771af66d6f24c3f6f695d4a9d2fc4a40f
Carlos Puche <cpuche@wepa.es>
2023-08-03 19:39:36 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use Wepa\Blog\Http\Controllers\Frontend\PostController;
 
 Route::prefix(config('blog.route_prefix', 'blog'))->middleware(['web', 'core.locale'])->group(function () {
-    Route::get('/{start_at?}', [PostController::class, 'index'])->name('blog');
+//    Route::get('/{start_at?}', [PostController::class, 'index'])->name('blog');
 });
